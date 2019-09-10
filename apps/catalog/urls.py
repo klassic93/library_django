16b7b0372f8093c71ca842ea079b731ee2058dc3
Petya <pro100dj93@mail.ru>
2019-09-10 19:59:37 +0300

    add books template

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from apps.catalog import views
+from django.conf.urls import url
+
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books')
 ]
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from apps.catalog.models import Book, Author, BookInstance
 
 
@@ -20,3 +21,14 @@ def index(request):
         'catalog/index.html',
         context=req_context
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    queryset = Book.objects.filter(language__name='English')
+    template_name = 'catalog/books/index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['another_data'] = 'These all English book in our library.'
+        return context
